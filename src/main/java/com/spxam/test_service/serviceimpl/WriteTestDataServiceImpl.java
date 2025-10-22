87199d1d6de8b6e0c85fb330581ebf520f65f4e0
Suman Paul <sum793763@gmail.com>
2025-10-22 05:33:26 +0530

    test service working

---
+++ src/main/java/com/spxam/test_service/serviceimpl/WriteTestDataServiceImpl.java
@@ -0,0 +1,37 @@
+package com.spxam.test_service.serviceimpl;
+
+import com.spxam.test_service.dto.test.CreateTestPayload;
+import com.spxam.test_service.entity.Test;
+import com.spxam.test_service.repo.IQuestionBankRepo;
+import com.spxam.test_service.repo.ITestRepo;
+import com.spxam.test_service.service.IWritetestDataService;
+import com.spxam.test_service.util.CommonUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class WriteTestDataServiceImpl implements IWritetestDataService {
+
+    private final IQuestionBankRepo iQuestionBankRepo;
+    private final ITestRepo iTestRepo;
+    @Override
+    public void createTestData(CreateTestPayload payload) {
+        Test test = Test.builder()
+                    .name(payload.name())
+                    .description(payload.description())
+                    .startTime(payload.startTime())
+                    .endTime(payload.endTime())
+                    .duration(payload.duration())
+                    .createdBy(payload.createdBy())
+                    .createdDate(CommonUtil.getCurrentDateTime())
+                    .isActive(true)
+                    .totalMark(payload.totalMarks())
+                    .build();
+        if(payload.questionBankId()!=null){
+            iQuestionBankRepo.findByIdAndIsActiveTrue(payload.questionBankId()).ifPresent(test::setQuestionBank);
+        }
+        iTestRepo.save(test);
+        return ;
+    }
+}
---
+++ src/main/java/com/spxam/test_service/util/CommonUtil.java
@@ -0,0 +1,21 @@
+package com.spxam.test_service.util;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class CommonUtil {
+	
+	public static boolean isEmptyString(String str) {
+		return str==null || str.isEmpty();
+	}
+	public static void validateMandatory(String value, String fieldName, List<String> errors) {
+		if (CommonUtil.isEmptyString(value)) {
+			errors.add(fieldName + " is mandatory.");
+		}
+	}
+
+	public static LocalDateTime getCurrentDateTime() {
+		return LocalDateTime.now();
+	}
+
+}
